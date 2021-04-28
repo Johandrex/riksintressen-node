2d7be2349571f9823ed0f7c1c9613647e49f366f
Johannes Seldevall <johandrex@protonmail.com>
2021-04-28 12:46:47 +0000

    add code in files types: .md,.js,.json remove code in files types:

---
+++ content.js
@@ -0,0 +1,179 @@
+/* express, REST API */
+const express = require('express');
+const app = express(); /* funktioner: app.get(), app.post(), app.put(), app.delete() */
+
+/* tillåter Cross-Origin Resource Sharing, alltså att andra webbsidor kommer åt API:n */
+const cors = require('cors');
+app.use(cors());
+
+// enable files upload
+const fileUpload = require('express-fileupload');
+const fs = require("fs");
+app.use(fileUpload({
+    createParentPath: true
+}));
+app.use(express.static("./uploads")); // Gör uploads offentligt för åtkomst, utanför express
+
+// middleware, for app.get()
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: false
+}));
+
+/* importera databas funktioner som används av api:n */
+const database = require("./database");
+const {
+    response,
+    static
+} = require('express');
+
+/* starta api */
+function start() {
+
+    /*********** GET ***********/
+
+    /* startsidan för API */
+    app.get('/', (req, res) => {
+        /* kommer åt webbsidan */
+        res.send('Welcome to our super API!');
+    });
+
+    /* hämtar listdata med alla riksintressen*/
+    app.get('/api/riksintressen/list', async (req, res) => {
+        const data = await database.getRiksintressenList();
+        res.setHeader("content-type", "application/json");
+        res.send(JSON.stringify(data));
+    });
+
+    /* hämtar listdata med alla raderade riksintressen*/
+    app.get('/api/riksintressen/list/deleted', async (req, res) => {
+        const data = await database.getRiksintressenListDeleted();
+        res.setHeader("content-type", "application/json");
+        res.send(JSON.stringify(data));
+    });
+
+    /* hämta specifikt riksintresse utifrån ID */
+    app.get('/api/riksintressen/:id', async (req, res) => {
+        const data = await database.getRiksintresse(req.params.id);
+        res.setHeader("content-type", "application/json");
+        res.send(JSON.stringify(data));
+    });
+
+    /* hämtar alla kommuner */
+    app.get('/api/kommuner', async (req, res) => {
+        const data = await database.getKommuner();
+        res.setHeader("content-type", "application/json");
+        res.send(JSON.stringify(data));
+    });
+
+    /* hämtar alla lan */
+    app.get('/api/lan', async (req, res) => {
+        const data = await database.getLan();
+        res.setHeader("content-type", "application/json");
+        res.send(JSON.stringify(data));
+    });
+
+    /* hämtar alla kommuner */
+    app.get('/api/kulturmiljotyper', async (req, res) => {
+        const data = await database.getKulturmiljotyp();
+        res.setHeader("content-type", "application/json");
+        res.send(JSON.stringify(data));
+    });
+
+
+    /* hämta ett riksintresses bilder/dokument */
+    app.get('/api/files/:id', async (req, res) => {
+        try {
+            const directoryPath = "uploads/" + req.params.id;
+
+            fs.readdir(directoryPath, function (err, files) {
+                let fileInfos = [];
+
+                if (files != undefined) { // kontrollera att filer finns, loopa då igenom de
+                    files.forEach((file) => {
+                        fileInfos.push({
+                            name: file,
+                            url: req.params.id + "/" + file,
+                        });
+                    });
+
+                    res.send(JSON.stringify(fileInfos));
+                } else if (err) { // ifall ett riksintresse inte har filer, informera om det.
+                    res.send({
+                        message: "There's no files associated with this riksintresse!",
+                    });
+                }
+            });
+        } catch (err) {
+            res.send(err);
+        }
+    });
+
+    /*********** POST ***********/
+
+    /* ladda upp dokument/bilder */
+    app.post('/api/upload', async (req, res) => {
+        try {
+            if (!req.files) {
+                res.send({
+                    status: false,
+                    message: 'No file uploaded'
+                });
+            } else {
+                let file = req.files.file;
+
+                //Use the mv() method to place the file in upload directory (i.e. "uploads")
+                console.log("File: " + file.name + " uploaded for id " + req.body.id);
+                file.mv('uploads/' + req.body.id + '/' + file.name);
+
+                //send response
+                res.send({
+                    status: true,
+                    message: 'File is uploaded',
+                    data: {
+                        name: file.name,
+                        mimetype: file.mimetype,
+                        size: file.size
+                    }
+                });
+            }
+        } catch (err) {
+            res.status(500).send(err);
+        }
+    });
+
+    /* uppdatera ett existerande riksintresse */
+    app.post('/api/update/riksintresse', async (req, res) => {
+        let message;
+
+        try {
+            message = await database.updateRiksintresse(req.body); // uppdatera objekt
+        } catch (exception) {
+            message = "Could not update riksintresse, exception: " + exception;
+        }
+
+        console.log(message);
+        res.send(JSON.stringify(message));
+    });
+
+    /* uppdatera ett existerande riksintresse */
+    app.post('/api/create/riksintresse', async (req, res) => {
+        let message;
+
+        try {
+            message = await database.createRiksintresse(req.body); // uppdatera objekt
+        } catch (exception) {
+            message = "Could not create riksintresse, exception: " + exception;
+        }
+
+        console.log(message.message + " " + message.id);
+        res.send(message);
+    });
+
+    app.listen(3000, () => console.log('Server started on port 3000')); // starta servern
+}
+
+module.exports = {
+    start: start
+}
+console.log("rtsqy")
---
+++ content.md
@@ -0,0 +1,26 @@
+# riksintressen-node
+
+Applikationsprogrammeringsgränssnittet fungerar som en länk mellan databasen och webbsidan, syftet är att webbsidan ska kunna lagra och hämta data från/till databasen. Den är byggd i node.js med ramverket Node Express. Applikationen är uppbyggd av filerna i api.js, database-pool.js, databas.js och index.js. 
+
+* index.js - startar databasen och API via database.js och api.js. 
+* database-pool.js - skapar en aktiv koppling till databasen, filen innehåller inloggningsuppgifter till databasen som ingen utomstående har åtkomst till. 
+* database.js - består av funktioner som utför SQL-queries mot databasen. 
+* api.js - öppnar upp portar som externa applikationer kommer åt. 
+
+För att illustrera hur applikationsprogrammeringsgränssnittets design och flöde fungerar kan vi använda ett exempel. Ifall en klient går in på webbsidan och öppnar upp listan med riksintressena skickas en API request från webbsidan till denna API. Då åkallas app.get(“/api/riksintressen/list”) i vår api.js. Funktionen utför await database.getRiksintressenList() som i database.js utför en SQL-query med kommandot pool.query("SELECT * FROM riksintresse ORDER BY id"). När queryn har utförts tar API:n emot resultatet och skickar vidare den till webbsidan. 
+
+## information
+
+riksintressen-node är uppbyggt i node.js och programmeringsspråket JavaScript. Projektet kräver ett flertal plugins som node express, pg, cors och express-fileupload för att fungera som avsett.
+
+Skapare: Johannes Seldevall, Sebastian Sjöberg och Wibke Du Rietz.
+
+## installation
+
+1. installera npm från sajten "https://www.npmjs.com/get-npm"
+2. klona repository med kommandot "git clone https://Johandrex@bitbucket.org/Johandrex/riksintressen-node.git"
+3. gå in i repository mappen "cd riksintressen-node"
+4. installera nödvändiga plugins via npm "npm install express pg cors express-fileupload"
+5. installera nodemon globalt ifall node applikationen ska köras konstant och uppdateras vid förändringar "sudo npm i -g nodemon"
+6. starta applikationen med "node index.js" eller "nodemon index.js"
+slfnhyrodb empqgdpdku tweajojehe
