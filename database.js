70cbe76e0880fb3c78fc79313a91632653866598
Johannes Seldevall <Johannes.Seldevall.0120@student.uu.se>
2021-05-03 12:40:44 +0200

    Gör ett register

--- database.js
+++ database.js
@@ -23,27 +23,67 @@ async function getRiksintressen() {
     }
 }
 
+/* hämta kulturmiljötyper kopplade till alla riksintressen */
+async function getRiksintressenKulturmiljotyper() {
+    try {
+        const results = await pool.query("SELECT * FROM riksintresse ORDER BY id");
+        return results.rows;
+    } catch(e) {
+        console.log("couldn't execute getRiksintressenKulturmiljotyp(), exception: " + e);
+        return [];
+    }
+}
 
-/*
-SELECT r.id, r.namn, r.beskrivning, r.motivering, r.version, r.geometri_id,
-	   kommun.namn as kommun, lan.namn as lan, kulturmiljotyp.namn as kulturmiljotyp, tidigare_version.id as tidigare_version_id FROM riksintresse as r
-INNER JOIN riksintresse_i_kommun AS rik ON rik.riksintresse_id = r.id
-INNER JOIN kommun ON rik.kommun_kod = kommun.kod
-INNER JOIN lan ON kommun.lan_kod = lan.kod
+/* hämta kommuner kopplade till alla riksintressen */
+async function getRiksintressenKommuner() {
+    try {
+        const results = await pool.query("SELECT kulturmiljotyp.id, kulturmiljotyp.namn from riksintresse_har_kulturmiljotyp" + 
+        " INNER JOIN kulturmiljotyp ON riksintresse_har_kulturmiljotyp.kulturmiljotyp_id = kulturmiljotyp.id" + 
+        " WHERE riksintresse_id = " + id + " ORDER BY riksintresse_id");
+        return results.rows;
+    } catch(e) {
+        console.log("couldn't execute getRiksintressenKommuner(), exception: " + e);
+        return [];
+    }
+}
 
-/* left join, då läggs riksintressen till trots att de inte har en kulturmiljötyp/tidigare version 
-LEFT JOIN riksintresse_har_kulturmiljotyp AS rhk ON rhk.riksintresse_id = r.id
-LEFT JOIN kulturmiljotyp ON rhk.kulturmiljotyp_id = kulturmiljotyp.id
+/* hämta de kommuner ett riksintresse ligger i */
+async function getKommuner() {
+    try {
+        const results = await pool.query("SELECT * FROM kommun;");
+        return results.rows;
+    } catch(e) {
+        console.log("couldn't execute getKommuner(), exception: " + e);
+        return [];
+    }
+}
 
-LEFT JOIN riksintresse_har_version AS rhv ON rhv.riksintresse_id = r.id
-LEFT JOIN tidigare_version ON rhv.tidigare_version_id = tidigare_version.id
-ORDER BY r.id;
-*/
+/* hämta de kommuner ett riksintresse ligger i */
+async function GetLan() {
+    try {
+        const results = await pool.query("SELECT * FROM lan");
+        return results.rows;
+    } catch(e) {
+        console.log("couldn't execute GetLan(), exception: " + e);
+        return [];
+    }
+}
 
-/* hämta ett riksintressen */
+/* hämta de kommuner ett riksintresse ligger i */
+async function GetKulturmiljotyp() {
+    try {
+        const results = await pool.query("SELECT * FROM kulturmiljotyp");
+        return results.rows;
+    } catch(e) {
+        console.log("couldn't execute GetKulturmiljotyp(id), exception: " + e);
+        return [];
+    }
+}
+
+/* hämta ett riksintresse */
 async function getRiksintresse(id) {
     try {
-        const results = await pool.query("SELECT * FROM riksintresse WHERE id = " + id);
+        const results = await pool.query("SELECT * FROM riksintresse WHERE riksintresse_id = " + id);
         return results.rows;
     } catch(e) {
         console.log("couldn't execute getRiksintresse(id), exception: " + e);
@@ -52,9 +92,36 @@ async function getRiksintresse(id) {
 }
 
 /* hämta ett riksintresses tidigare versioner */
-async function getHistorik(id) {
+async function getRiksintresseHistorik(id) {
     try {
         const results = await pool.query("SELECT * FROM riksintresse_har_version WHERE riksintresse_id = " + id);
+        return results.rows;
+    } catch(e) {
+        console.log("couldn't execute getRiksintresseHistorik(id), exception: " + e);
+        return [];
+    }
+}
+
+/* hämta ett riksintresses kulturmiljötyper */
+async function getRiksintresseKulturmiljo(id) {
+    try {
+        const results = await pool.query("SELECT kulturmiljotyp.id, kulturmiljotyp.namn from riksintresse_har_kulturmiljotyp" + 
+        " INNER JOIN kulturmiljotyp ON riksintresse_har_kulturmiljotyp.kulturmiljotyp_id = kulturmiljotyp.id" + 
+        " WHERE riksintresse_id = " + id + " ORDER BY riksintresse_id");
+        return results.rows;
+    } catch(e) {
+        console.log("couldn't execute getRiksintresseKulturmiljö(id), exception: " + e);
+        return [];
+    }
+}
+
+/* hämta de kommuner ett riksintresse ligger i */
+async function getRiksintresseKommuner(id) {
+    try {
+        const results = await pool.query("SELECT kommun.namn as Kommun, lan.namn as Lan FROM riksintresse_i_kommun" +
+            " INNER JOIN kommun ON kommun.kod = riksintresse_i_kommun.kommun_kod" +
+            " INNER JOIN lan ON lan.kod = kommun.lan_kod" +
+            " WHERE riksintresse_id = 1;");
         return results.rows;
     } catch(e) {
         console.log("couldn't execute getHistorik(id), exception: " + e);
@@ -87,8 +154,12 @@ async function getGeometri(id) {
 module.exports = {
     connect: connect,
     getRiksintressen: getRiksintressen,
+
+    getKommuner: getKommuner,
+    GetLan: GetLan,
+    GetKulturmiljotyp: GetKulturmiljotyp,
+
     getRiksintresse: getRiksintresse,
-    getHistorik: getHistorik,
     getGeometrier: getGeometrier,
     getGeometri: getGeometri
 }
